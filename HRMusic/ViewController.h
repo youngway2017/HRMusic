d91267d084677ccf91dfc22ba25f91c6bf4b030d
Yangwei <654183746@qq.com>
2015-12-28 17:07:53 +0800

    Initial Commit

---
+++ HRMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HRMusic
+//
+//  Created by Yangwei on 15/12/28.
+//  Copyright © 2015年 Yangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
